b62db18ab8e867b0404418cd501b646add7a3993
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-04-17 10:52:04 +0600

    added navbar and footer and component file

---
+++ src/components/Header/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li className="m-2">Home</li>
+      <li className="m-2">About</li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <a className="text-3xl font-bold">Book Vibe</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end flex items-center gap-3">
+        <button className="btn bg-[#23BE0A] text-white border-none">Sign In</button>
+        <button className="btn bg-[#59C6D2] text-white border-none">Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
